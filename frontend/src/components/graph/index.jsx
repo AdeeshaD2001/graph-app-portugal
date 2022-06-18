16f676ad341979108e947c9e10c810643b822a41
ManupaDev <manupadevsuite@gmail.com>
2022-06-18 15:31:07 +0530

    added console.log() for dataChart

--- frontend/src/components/graph/index.jsx
+++ frontend/src/components/graph/index.jsx
@@ -3,7 +3,7 @@ import { VictoryScatter, VictoryChart } from "victory";
 export default function AreaChart({ maxDomain, width, height, data }) {
     if(data){
         const dataChart = data;
-               
+        console.log(dataChart);       
         return (
             <div>
                 <VictoryChart >
