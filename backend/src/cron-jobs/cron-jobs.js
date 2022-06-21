5c7349c6dfc8cd41872abc9b6367d1387eed680f
ManupaDev <manupadevsuite@gmail.com>
2022-06-21 21:10:19 +0530

    added comments for cron job

--- backend/src/cron-jobs/cron-jobs.js
+++ backend/src/cron-jobs/cron-jobs.js
@@ -3,19 +3,20 @@ import User from "../models/User";
 
 const cronTest = function () {
     
-    cron.schedule('* * * * * *', function() {
+    cron.schedule('* * * * * *', function() { // node-cron test function.
         console.log('Running a task every second');
     });
 }
 
-const validateSubscription = async function(){
+const validateSubscription = async function(){ // utility funtion to validate a subscriptionDuration
     try{
-        let users = await  User.find({isSubscribed: true});
-        users.forEach(async function(user) {
+        let users = await  User.find({isSubscribed: true});// fetch all the users in the database with active subscriptions.
+        users.forEach(async function(user) {// iterate over all the subscribed users to verify the validity.
             console.log(Date.now() - user.subscriptionDuration.getTime());
-            if (Date.now() - user.subscriptionDuration.getTime() > 0) {
+            if (Date.now() - user.subscriptionDuration.getTime() > 0) {// check whether the user subscription is expired by checking the unix timestamps of the subscription end date and the current time.
                 console.log(`${user.name} subscription expired.`);
                 const nuser = await User.findByIdAndUpdate(user._id, {isSubscribed: false, subscriptionType:null, subscriptionDuration: null, chosenChains:null, visitorId:null}, {new: true});
+                //update the user in the database with the expired subscription state field values
                 console.log(nuser);
             }
         });
@@ -26,6 +27,8 @@ const validateSubscription = async function(){
 
 const subscriptionAutomation = function () {
     
+    // node-cron schedule function that is initiated when the server first goes live
+    //Will trigger the validation function everyday at 00:00AM.
     cron.schedule('0 0 * * *', function() {
         console.log('Running a task at 00:00 AM every day.');
         validateSubscription();
