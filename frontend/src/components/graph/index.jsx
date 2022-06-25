ce86dca60617a27db307f2a8af904e0770071197
W A A Dissanayake <waadissanayake@gmail.com>
2022-06-25 12:26:40 +0530

    Working Graphical Visualization

--- frontend/src/components/graph/index.jsx
+++ frontend/src/components/graph/index.jsx
@@ -1,21 +1,38 @@
-import { VictoryScatter, VictoryChart } from "victory";
+import { VictoryScatter, VictoryChart, VictoryTheme, VictoryTooltip } from "victory";
 
-export default function AreaChart({ maxDomain, width, height, data }) {
+export default function AreaChart({ max_x, max_y, consumoAverage, data }) {
     if(data){
         const dataChart = data;
-        console.log(dataChart);       
+        const dataMax_x = max_x;
+        const dataMax_y = max_y;
+        const dataConsumoAverage = consumoAverage;
+        console.log(dataChart);    
+        console.log(dataMax_x);       
+        console.log(dataMax_y);    
         return (
             <div>
-                <VictoryChart >
-                  <VictoryScatter />
-                </VictoryChart>    
+                <VictoryChart
+                  width={1520} height={710}
+                  theme={VictoryTheme.material}
+                  domain={{ x: [0, dataMax_x], y: [0, dataMax_y] }}
+                >
+                  <VictoryScatter
+                    labelComponent={<VictoryTooltip/>}
+                    style={{ data: { fill: ({ datum }) => datum.y > dataConsumoAverage
+                    ? "#FF0000"
+                    : "#000CFE" } }}
+                    size={7}
+                    data={dataChart}
+                  />
+                </VictoryChart>   
             </div>
-            
         )
     }   
-  return (
-    <div>
-      <span>Não há lojas cadastradas.</span>
-    </div>
-  );
+    else {
+      return (
+      <div>
+        <span>Não há lojas cadastradas.</span>
+      </div>
+    );
+    }
 }
